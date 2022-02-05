640bf636e676b1bf2ffffcf5b8fce94c9034a07b
Mukur Panchani <panchanimukur@gmail.com>
2022-02-05 19:42:33 +0530

    remove twitter meta for now

--- components/Container.tsx
+++ components/Container.tsx
@@ -39,8 +39,8 @@ export default function Container(props) {
   const router = useRouter();
   const meta = {
     title: 'Mukur Panchani',
-    description: `Platform Engineer & Open Source enthusiast.`,
-    image: 'https://leerob.io/static/images/banner.png',
+    description: `Cloud Engineer & Developer.`,
+    image: 'https://mukur.dev/static/images/banner.png',
     type: 'website',
     ...customMeta
   };
@@ -51,18 +51,13 @@ export default function Container(props) {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
-        <link rel="canonical" href={`https://leerob.io${router.asPath}`} />
+        <meta property="og:url" content={`https://mukur.dev${router.asPath}`} />
+        <link rel="canonical" href={`https://mukur.dev${router.asPath}`} />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Lee Robinson" />
+        <meta property="og:site_name" content="Mukur Panchani" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@leeerob" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
         {meta.date && (
           <meta property="article:published_time" content={meta.date} />
         )}
