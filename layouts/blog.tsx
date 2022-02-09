7731387acb5f0aeabd8e7d3a68f1529ab34cd1b6
Mukur Panchani <panchanimukur@gmail.com>
2022-02-09 13:56:32 +0530

    Update blog layout

--- layouts/blog.tsx
+++ layouts/blog.tsx
@@ -7,10 +7,10 @@ import type { PropsWithChildren } from 'react';
 import type { Blog } from '.contentlayer/types';
 
 const editUrl = (slug) =>
-  `https://github.com/leerob/leerob.io/edit/main/data/blog/${slug}.mdx`;
+  `https://github.com/3sne/lz/edit/main/data/blog/${slug}.mdx`;
 const discussUrl = (slug) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(
-    `https://leerob.io/blog/${slug}`
+    `https://mukur.dev/blog/${slug}`
   )}`;
 
 export default function BlogLayout({
@@ -19,9 +19,9 @@ export default function BlogLayout({
 }: PropsWithChildren<{ post: Blog }>) {
   return (
     <Container
-      title={`${post.title} – Lee Robinson`}
+      title={`${post.title} – Mukur Panchani`}
       description={post.summary}
-      image={`https://leerob.io${post.image}`}
+      image={`https://mukur.dev${post.image}`}
       date={new Date(post.publishedAt).toISOString()}
       type="article"
     >
@@ -32,21 +32,19 @@ export default function BlogLayout({
         <div className="flex flex-col items-start justify-between w-full mt-2 md:flex-row md:items-center">
           <div className="flex items-center">
             <Image
-              alt="Lee Robinson"
+              alt="Mukur Panchani"
               height={24}
               width={24}
               src="/avatar.jpg"
               className="rounded-full"
             />
             <p className="ml-2 text-sm text-gray-700 dark:text-gray-300">
-              {'Lee Robinson / '}
+              {'Mukur Panchani / '}
               {format(parseISO(post.publishedAt), 'MMMM dd, yyyy')}
             </p>
           </div>
           <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 min-w-32 md:mt-0">
             {post.readingTime.text}
-            {` • `}
-            <ViewCounter slug={post.slug} />
           </p>
         </div>
         <div className="w-full mt-4 prose dark:prose-dark max-w-none">
