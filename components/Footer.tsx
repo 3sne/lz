0960733a1fee0aa6aafe24701d2c795ed6463308
3sne <panchanimukur@gmail.com>
2022-07-17 19:08:12 +0530

    footer: divide by sections for readability

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,19 +20,52 @@ export default function Footer() {
       {/* <NowPlaying /> */}
       <div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
         <div className="flex flex-col space-y-4">
-          <Link href="/">
-            <a className="text-gray-500 hover:text-gray-600 transition">Home</a>
-          </Link>
+          <h4 className="text-lg font-medium w-full text-gray-800 dark:text-gray-100">
+            Content.
+          </h4>
           <Link href="/about">
             <a className="text-gray-500 hover:text-gray-600 transition">
               About
             </a>
           </Link>
-          <Link href="/uses">
-            <a className="text-gray-500 hover:text-gray-600 transition">Uses</a>
+          <Link href="/blog">
+            <a className="text-gray-500 hover:text-gray-600 transition">Blog</a>
+          </Link>
+          <Link href="/experience">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              Experience
+            </a>
+          </Link>
+          <Link href="/connect">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              Connect
+            </a>
           </Link>
         </div>
         <div className="flex flex-col space-y-4">
+          <h4 className="text-lg font-medium w-full text-gray-800 dark:text-gray-100">
+            Fun Stuff.
+          </h4>
+          <Link href="/uses">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              WFH Setup
+            </a>
+          </Link>
+          <Link href="/snippets">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              Snippets
+            </a>
+          </Link>
+          <Link href="/xkcd">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              xkcd.
+            </a>
+          </Link>
+        </div>
+        <div className="flex flex-col space-y-4">
+          <h4 className="text-lg font-medium w-full text-gray-800 dark:text-gray-100">
+            React Out!
+          </h4>
           <ExternalLink href="https://github.com/3sne">GitHub</ExternalLink>
           <ExternalLink href="https://in.linkedin.com/in/mukur-panchani-295803183">
             LinkedIn
@@ -40,21 +73,9 @@ export default function Footer() {
           <ExternalLink href="https://medium.com/@panchanimukur">
             Medium
           </ExternalLink>
-        </div>
-        <div className="flex flex-col space-y-4">
-          <Link href="/uses">
-            <a className="text-gray-500 hover:text-gray-600 transition">Uses</a>
-          </Link>
-          <Link href="/snippets">
-            <a className="text-gray-500 hover:text-gray-600 transition">
-              Snippets
-            </a>
-          </Link>
-          <Link href="/tweets">
-            <a className="text-gray-500 hover:text-gray-600 transition">
-              Tweets
-            </a>
-          </Link>
+          <ExternalLink href="mailto:panchanimukur@gmail.com">
+            Email
+          </ExternalLink>
         </div>
       </div>
     </footer>
