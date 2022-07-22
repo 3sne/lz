08b24230c873363b74dfcdfcbc99ab1c25b48ad0
3sne <panchanimukur@gmail.com>
2022-07-23 01:23:35 +0530

    update /uses page

--- layouts/uses.tsx
+++ layouts/uses.tsx
@@ -3,17 +3,15 @@ import Container from 'components/Container';
 export default function UsesLayout({ children }) {
   return (
     <Container
-      title="Uses – Lee Robinson"
-      description="Here's what tech I'm currently using for coding, videos, and music."
+      title="Uses – Mukur Panchani"
+      description="Here's my WFH setup."
     >
       <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          My Gear
+          WFH Setup
         </h1>
-        <p className="text-gray-700 dark:text-gray-300 mt-2 mb-8">
-          Here's what tech I'm currently using for coding, videos, and music.
-          Most of these have been accumulated over the past few years, with a
-          recent office upgrade in 2020.
+        <p className="text-gray-700 dark:text-gray-300 mt-2">
+          Here's some humble tech I have been using since 2020 to WFH.
         </p>
         <div className="prose dark:prose-dark w-full">{children}</div>
       </article>
